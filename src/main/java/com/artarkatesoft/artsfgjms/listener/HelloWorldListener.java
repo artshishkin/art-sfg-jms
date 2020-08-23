36eeead100fe761f3f562fc9be0653f66de762d2
admin <d.art.shishkin@gmail.com>
2020-08-24 00:27:08 +0300

    Send and Receive of JMS Messages (shorter form) (459). closes #8

--- src/main/java/com/artarkatesoft/artsfgjms/listener/HelloWorldListener.java
+++ src/main/java/com/artarkatesoft/artsfgjms/listener/HelloWorldListener.java
@@ -4,21 +4,17 @@ import com.artarkatesoft.artsfgjms.config.JmsConfig;
 import com.artarkatesoft.artsfgjms.model.HelloWorldMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
-import javax.jms.JMSException;
 import javax.jms.Message;
 import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
 public class HelloWorldListener {
-
-    private final JmsTemplate jmsTemplate;
 
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listen(@Payload HelloWorldMessage helloWorldMessage, @Headers MessageHeaders messageHeaders, Message message) {
@@ -30,12 +26,11 @@ public class HelloWorldListener {
     }
 
     @JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
-    public void listenAndReply(@Payload HelloWorldMessage helloWorldMessage, @Headers MessageHeaders messageHeaders, Message message) throws JMSException {
+    public HelloWorldMessage listenAndReply(@Payload HelloWorldMessage helloWorldMessage) {
 
-        HelloWorldMessage replyMessage = HelloWorldMessage.builder()
+        return HelloWorldMessage.builder()
                 .id(UUID.randomUUID())
                 .message("And you too [" + helloWorldMessage.getMessage().toUpperCase() + "]")
                 .build();
-        jmsTemplate.convertAndSend(message.getJMSReplyTo(), replyMessage);
     }
 }
