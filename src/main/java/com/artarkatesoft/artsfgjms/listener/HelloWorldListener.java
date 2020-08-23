1f2e38e5faf95f413b42178205dc3353bfda0b1b
admin <d.art.shishkin@gmail.com>
2020-08-24 00:22:22 +0300

    Send and Receive of JMS Messages (459). #8

--- src/main/java/com/artarkatesoft/artsfgjms/listener/HelloWorldListener.java
+++ src/main/java/com/artarkatesoft/artsfgjms/listener/HelloWorldListener.java
@@ -2,23 +2,40 @@ package com.artarkatesoft.artsfgjms.listener;
 
 import com.artarkatesoft.artsfgjms.config.JmsConfig;
 import com.artarkatesoft.artsfgjms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
 import javax.jms.Message;
+import java.util.UUID;
 
 @Component
+@RequiredArgsConstructor
 public class HelloWorldListener {
+
+    private final JmsTemplate jmsTemplate;
 
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listen(@Payload HelloWorldMessage helloWorldMessage, @Headers MessageHeaders messageHeaders, Message message) {
 
-        System.out.println("Got a message");
-        System.out.println(helloWorldMessage);
+//        System.out.println("Got a message");
+//        System.out.println(helloWorldMessage);
 
 //        throw new RuntimeException("not finished transaction");
+    }
+
+    @JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
+    public void listenAndReply(@Payload HelloWorldMessage helloWorldMessage, @Headers MessageHeaders messageHeaders, Message message) throws JMSException {
+
+        HelloWorldMessage replyMessage = HelloWorldMessage.builder()
+                .id(UUID.randomUUID())
+                .message("And you too [" + helloWorldMessage.getMessage().toUpperCase() + "]")
+                .build();
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), replyMessage);
     }
 }
--- src/main/java/com/artarkatesoft/artsfgjms/sender/HelloSender.java
+++ src/main/java/com/artarkatesoft/artsfgjms/sender/HelloSender.java
@@ -1,20 +1,27 @@
 package com.artarkatesoft.artsfgjms.sender;
 
 import com.artarkatesoft.artsfgjms.model.HelloWorldMessage;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
 import java.util.UUID;
 
 import static com.artarkatesoft.artsfgjms.config.JmsConfig.MY_QUEUE;
+import static com.artarkatesoft.artsfgjms.config.JmsConfig.MY_SEND_RCV_QUEUE;
 
 @Component
 @RequiredArgsConstructor
 public class HelloSender {
 
     private final JmsTemplate jmsTemplate;
+    private final ObjectMapper objectMapper;
 
     @Scheduled(fixedRate = 2000)
     private void send() {
@@ -26,7 +33,31 @@ public class HelloSender {
 
         jmsTemplate.convertAndSend(MY_QUEUE, message);
 
-        System.out.println("Message sent");
+//        System.out.println("Message sent");
+    }
+
+    @Scheduled(fixedRate = 1500)
+    private void sendAndReceive() throws JMSException {
+
+        HelloWorldMessage messageSent = HelloWorldMessage.builder()
+                .id(UUID.randomUUID())
+                .message("Hello there")
+                .build();
+        System.out.println("Message sent: " + messageSent);
+
+        Message messageReceived = jmsTemplate.sendAndReceive(MY_SEND_RCV_QUEUE, session -> {
+            try {
+                TextMessage helloMessage = session.createTextMessage(objectMapper.writeValueAsString(messageSent));
+                helloMessage.setStringProperty("_type", HelloWorldMessage.class.getName());
+                return helloMessage;
+            } catch (JsonProcessingException e) {
+                throw new JMSException(e.getMessage());
+            }
+        });
+
+        System.out.println("Message received back: " + messageReceived.getBody(String.class));
+
+
     }
 
 }
